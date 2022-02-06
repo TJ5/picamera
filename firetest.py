d0b03dfd13000e14160d7ed1765a9f3e3e062a1b
Tejesh Anand <tjanand@usc.edu>
2022-02-05 20:54:43 -0800

    camera

--- firetest.py
+++ firetest.py
@@ -1,25 +1,20 @@
 from firebase_admin import credentials, initialize_app, storage
-import socket
-from PIL import Image
-import time
-
+import os
 
 cred = credentials.Certificate("hacksc2022-4d22b-firebase-adminsdk-pymt0-852e6c81ac.json")
 initialize_app(cred, {'storageBucket': 'hacksc2022-4d22b.appspot.com'})
 fileName = 'latest.jpg'
 
 
-uploadImage = False
+uploadImage = True
 
 #To be defined
 
 if (uploadImage):
-    timestamp = time.time()
+    os.system("raspistill -o latest.jpg")
     bucket = storage.bucket()
     blob = bucket.blob(fileName)
     blob.upload_from_filename(fileName)
     blob.make_public()
     
-
-
 
